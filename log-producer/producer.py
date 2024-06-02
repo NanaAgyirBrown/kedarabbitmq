0494c8abc1309a1dbef5ff4702002836fd599bd5
Nana Agyir Brown <nanabrown.agyir@gmail.com>
2024-06-02 19:54:30 -0400

    updated - based on topics

--- log-producer/producer.py
+++ log-producer/producer.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import random
 
 import pika
@@ -30,8 +31,26 @@ def produce_logs(counter):
     connection.close()
 
 
+def produce_topics(counter):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+    routing_keys = os.environ['ROUTING_KEYS'].split(',')
+
+    key = random.choice(routing_keys)
+    routing_key = key if len(key.split('.')) > 1 else 'anonymous.info'
+
+    message = ' '.join(sys.argv[2:]) or f'{counter} : This is a topic emission for {routing_key}'
+
+    channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
+
+    print(f" [x] broadcasted : {message}")
+    connection.close()
+
 if __name__ == '__main__':
     counter = 0
     while True:
         counter += 1
         produce_logs(counter)
+        produce_topics(counter)
